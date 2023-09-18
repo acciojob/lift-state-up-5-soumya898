34f90bb59dc52aaf4d37650cd0436a1b2b7a8121
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-09-19 02:23:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [username, setUserName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [dataList, setDataList] = React.useState(false);
+
+  function handleLogin() {
+    setDataList('User logged in!');
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input
+        name='username'
+        type='text'
+        onChange={(e) => setUserName(e.target.value)}
+      />{" "}
+      <br></br>
+      <input
+        name='email'
+        type='email'
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login </button>
+
+      {dataList && (
+        <p> {dataList}</p>
+      )}
     </div>
   )
 }
 
-export default App
+export default App;
